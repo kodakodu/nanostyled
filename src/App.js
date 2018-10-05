7fb65abc24ee59ab0b4edff4f75a03a1c54de5fa
kodakodu <kodakodu@gmail.com>
2018-10-05 11:58:50 +0200

    _

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import nanostyled from 'nanostyled';
+import 'tachyons/css/tachyons.css';
+
+const Button = nanostyled('button',{
+  color: 'white',
+  bg: 'bg-blue',
+  base: 'fw7 br3 pa2 sans-serif f4 bn input-reset'
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button>Base Button</Button>
+        <Button bg='bg-yellow'>Yellow Button</Button>
       </div>
     );
   }
